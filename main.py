b9cf758201c568c4496b34b4ee0bf486cf3f0509
Subhradipta Paul <101445022+SDP010@users.noreply.github.com>
2022-07-01 13:00:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -106,9 +106,9 @@ def prediction_route():
 #     httpd.serve_forever()
     # app.run()
     
-if __name__=='__main__':
-    app.run(debug=True,port=os.getenv("PORT"))
-    
-    
 # if __name__=='__main__':
-#     app.run(host="0.0.0.0",debug=True,port=os.environ['PORT'])
+#     app.run(debug=True,port=os.getenv("PORT"))
+    
+    
+if __name__=='__main__':
+    app.run(host="0.0.0.0",debug=True,port=os.environ['PORT'])
